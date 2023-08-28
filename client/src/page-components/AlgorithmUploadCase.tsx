c1c9f9456853869d2ca55dc024cf28d37cd9d195
guidoenr <guidoenr4@gmail.com>
2023-08-28 16:55:10 -0300

    clean up

--- client/src/page-components/AlgorithmUploadCase.tsx
+++
@@ -1,8 +0,0 @@
-
-export function AlgorithmUploadCase() {
-  return (
-    <div className="flex drop-shadow bg-white p-3 mb-6 rounded-md">
-        Hi how are you
-    </div>
-  );
-}
--- client/src/pages/algorithms/uploadAlgorithms/AlgorithmsUpload.tsx
+++ client/src/pages/algorithms/uploadAlgorithms/AlgorithmsUpload.tsx
@@ -8,7 +8,6 @@ export function AlgorithmsUpload() {
   return (
     <div>
       <DefaultLayout column={true}>
-      {/* <ScrambleDisplay height="1px" event="444"></ScrambleDisplay> */}
       <h1 className="text-4xl py-6 text-accent-dark font-bold">Upload your Algorithm</h1>
       <CubeSelectorPanel></CubeSelectorPanel>
       <ResolutionInput></ResolutionInput>   
