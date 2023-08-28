0acb89dee927e5b6f40b795f6f4bfd3263bddf79
crisszkutnik <crisszkutnik@gmail.com>
2023-08-28 00:15:12 -0300

    Minor fixes and close on backdrop click

--- client/src/components/navbar/UserActionButton.tsx
+++ client/src/components/navbar/UserActionButton.tsx
@@ -15,14 +15,19 @@ export function UserActionButton() {
     setOpen(!open);
   };
 
+  const clickAction = (path: string) => {
+    navigate(path);
+    setOpen(false);
+  };
+
   const values = [
     {
       text: "My info",
-      onClick: () => navigate("/userinfo"),
+      onClick: () => clickAction("/userinfo"),
     },
     {
       text: "My solves",
-      onClick: () => {},
+      onClick: () => clickAction("/userinfo/solves"),
     },
   ];
 
@@ -35,41 +40,52 @@ export function UserActionButton() {
   }, [dropdownRef, open]);
 
   return (
-    <div
-      className={
-        "flex h-full items-center relative pr-3" + (open ? " drop-shadow" : "")
-      }
-    >
-      <hr className="h-5/6 rounded w-px bg-black/5" />
-      <button
-        onClick={onClick}
-        className="ml-2 flex items-center cursor-pointer"
+    <>
+      <div
+        className={
+          "flex h-full items-center relative pr-3 z-50" +
+          (open ? " drop-shadow" : "")
+        }
       >
-        <FontAwesomeIcon icon={faUser} />
-        <p className="mx-3">Unknown user</p>
-        <FontAwesomeIcon icon={open ? faCaretDown : faCaretUp} />
-      </button>
+        <hr className="h-5/6 rounded w-px bg-black/5" />
+        <button
+          onClick={onClick}
+          className="ml-2 flex items-center cursor-pointer"
+        >
+          <FontAwesomeIcon icon={faUser} />
+          <p className="mx-3">Unknown user</p>
+          <FontAwesomeIcon icon={open ? faCaretDown : faCaretUp} />
+        </button>
+        {open && (
+          <div
+            ref={dropdownRef}
+            style={{ bottom: `-${dropdownMargin}px` }}
+            className="flex absolute bg-white flex-col w-full bottom-0"
+          >
+            {values.map(({ text, onClick }, index) => (
+              <button
+                key={index}
+                className={
+                  "py-2 hover:bg-accent-primary hover:text-white" +
+                  (index !== values.length - 1
+                    ? " border-b border-black/5"
+                    : "")
+                }
+                onClick={onClick}
+              >
+                {text}
+              </button>
+            ))}
+            <WalletDisconnectButton />
+          </div>
+        )}
+      </div>
       {open && (
         <div
-          ref={dropdownRef}
-          style={{ bottom: `-${dropdownMargin}px` }}
-          className="flex absolute bg-white flex-col w-full bottom-0"
-        >
-          {values.map(({ text, onClick }, index) => (
-            <button
-              key={index}
-              className={
-                "py-2 hover:bg-accent-primary hover:text-white" +
-                (index !== values.length - 1 ? " border-b border-black/5" : "")
-              }
-              onClick={onClick}
-            >
-              {text}
-            </button>
-          ))}
-          <WalletDisconnectButton />
-        </div>
+          className="absolute bottom-0 left-0 w-full h-full z-40"
+          onClick={onClick}
+        ></div>
       )}
-    </div>
+    </>
   );
 }
