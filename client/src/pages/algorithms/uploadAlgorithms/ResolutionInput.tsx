0d058d3f9d978d37ea83e23fbcdfc2adf6733a71
guidoenr <guidoenr4@gmail.com>
2023-08-29 16:09:40 -0300

    new verifiy button + changes to the ResolutionInput

--- client/src/pages/algorithms/uploadAlgorithms/ResolutionInput.tsx
+++ client/src/pages/algorithms/uploadAlgorithms/ResolutionInput.tsx
@@ -1,26 +1,48 @@
+import { useState } from "react";
 import { TextField, Button } from "@mui/material";
 import "./ResolutionInput.css";
 
 
 export function ResolutionInput() {
+  const [isVerified, setIsVerified] = useState(false);
+
+  const handleVerify = () => {
+  
+    setIsVerified(true);
+  };
+
+  const handleUpload = () => {
+    if (isVerified) {
+      // TODO: if verified, upload, how? IDK
+    } else {
+      // TODO: if not? IDK as well
+    }
+  };
+
   return (
     <div>
-      <div style={{ display: "grid", placeItems: "end" }}>
-        <TextField
-          id="outlined-basic"
-          label="Solve the cube!"
-          variant="outlined"
-          className="custom-textfield"
-          style={{ width: "800px", height: "100px", marginTop: "-500px" }}
-        />
-      </div>
-      <div style={{ display: "flex", justifyContent: "center", marginTop: "16px" }}>
-        <Button color="secondary" size="large" variant="outlined">
-          Verify
-        </Button>
-        <Button color="success" size="large" variant="contained" style={{ marginLeft: "40px" }}>
-          Upload
-        </Button>
+      <div className="flex flex-col w-4/4 ml-6" style={{marginTop: "-400px"}}>
+        <div style={{ display: "grid", placeItems: "end" }}>
+        <div className="note">
+        You need to provide a <b>cube-solving algorithm;</b> otherwise, the upload will not be possible.
+        </div>
+          <TextField
+            id="outlined-basic"
+            label="Solve the cube!"
+            variant="outlined"
+            className="custom-textfield"
+            style={{ width: "800px", height: "100px" }}
+          />
+        </div>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "16px" }}>
+          <Button color="secondary" size="large" variant="outlined" onClick={handleVerify}>
+            Verify
+          </Button>
+          <Button color="success" size="large" variant="contained" style={{ marginLeft: "40px" }} onClick={handleUpload} disabled={!isVerified}>
+            Upload
+          </Button>
+        </div>
+        
       </div>
     </div>
   );
