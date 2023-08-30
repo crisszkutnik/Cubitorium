5011500a1849dba9107733e88793f7de5a9ff657
guidoenr <guidoenr4@gmail.com>
2023-08-30 17:11:45 -0300

    auto CR

--- client/src/pages/algorithms/uploadAlgorithms/ResolutionInput.tsx
+++ client/src/pages/algorithms/uploadAlgorithms/ResolutionInput.tsx
@@ -20,9 +20,9 @@ export function ResolutionInput() {
     <div className="w-1/2 px-4 py-10 ml-96 -mt-96">
       <div className="w-full p-8 bg-white rounded-lg shadow-lg">
         <div className="mb-10 grid">
-          <div className="note">
-            You need to provide a <b>cube-solving algorithm;</b> otherwise, the upload will not be possible.
-          </div>
+        <div className="note text-xs text-gray-600 text-center tracking-wide leading-5 mb-5 bg-purple-100 p-2">
+        You need to provide a <b className="font-semibold">cube-solving algorithm;</b> otherwise, the upload will not be possible.
+        </div>
           <textarea
             className="custom-textarea border rounded-lg p-2 w-full h-20 text-lg tracking-wide"
             placeholder="Solve the cube!"
