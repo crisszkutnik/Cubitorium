d14627987c99852224894685da441c24c387cc1d
guidoenr <guidoenr4@gmail.com>
2023-09-25 17:09:59 -0300

    accordions

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,11 +1,6 @@
 import { Accordion, AccordionItem } from "@nextui-org/react";
 
 export function Home() {
-  const listItems = [
-    "Improve your cube-solving skills",
-    "Learn about new algorithms",
-    "Contribute to the community creating your own algorithms",
-  ];
 
   return (
     <div className="column">
@@ -20,21 +15,48 @@ export function Home() {
       <h3 className="text-2xl py-6 text-accent-dark text-center -mt-3">
         Experience the best way to...
       </h3>
-          <Accordion variant="splitted" className="mx-auto text-center column" style={{ maxWidth: "400px" }} disabledKeys={["4"]} >
-            <AccordionItem key="1" aria-label="Accordion 1" title="Improve your cube-solving skills">
-                asdasd
+      {/* por favor cristobal perdoname este inline-styling por favor te lo pido no lo pude hacer andar con max-w-400 por favor te lo pido */}
+      {/* te lo pido por favor.... por favor te lo pido */}
+          <Accordion variant="splitted" className="mx-auto text-center" style={{maxWidth:"370px"}}disabledKeys={["4"]} >
+        
+            <AccordionItem key="1" aria-label="2" title={
+                <span>
+                    Improve your <span className="text-green-700 font-bold">cube-solving skills</span>
+                </span>
+                }>
+
+                <p className="text-sm text-left text-gray-500 mr-40 -mt-3 mb-2">
+                Learning from others Speedcubers solutions 
+                </p>
             </AccordionItem>
-            <AccordionItem key="2" aria-label="Accordion 2" title="Learn about new algorithms" >
-                asdasda
+
+            <AccordionItem key="2" aria-label="2" title={
+                <span>
+                    Learn about <span className="text-purple-600 font-bold">new algorithms</span>
+                </span>
+                }>
+                <p className="text-sm text-left text-gray-500 mr-40 -mt-3 mb-2">
+                    Checking our great <b>Algorithms</b> page in which you could find all the uploaded algorithms by the community.
+                </p>
             </AccordionItem>
-            <AccordionItem key="3" aria-label="Accordion 3" title="Contribute to the community creating your own algorithms" >
+
+            <AccordionItem key="3" aria-label="3" title={
+                <span>
+                    Contribute to the <span className="text-blue-600 font-bold">community</span>
+                </span>
+                }>
+                <p className="text-sm text-left text-gray-500 mr-40 -mt-3 mb-2">
+                    Checking our great <b>Algorithms</b> page in which you could find all the uploaded algorithms by the community.
+                </p>
+            </AccordionItem>
+
+          
+
+            <AccordionItem key="4" aria-label="Accordion 4" title="we also sell drugs" >
               3
             </AccordionItem>
-            <AccordionItem key="4" aria-label="Accordion 4" title="Something that we don't provide or is in TODO status? IDK " >
-              3
-            </AccordionItem>
+
           </Accordion>
-        </div>
-      
+          </div>
   );
 }
