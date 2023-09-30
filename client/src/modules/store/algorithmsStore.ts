86ae37d6575194c9eaa8a0788263f23ea9554c31
crisszkutnik <crisszkutnik@gmail.com>
2023-09-30 03:07:43 -0300

    Replaced static data for dynamic data

--- client/src/modules/store/algorithmsStore.ts
+++ client/src/modules/store/algorithmsStore.ts
@@ -89,7 +89,7 @@ export const useAlgorithmsStore = createWithEqualityFn<UseAlgorithmsStoreState>(
   Object.is,
 );
 
-export function getPuzzleType(set: string) {
+export function getPuzzleType(set: string): PuzzleTypeKey {
   const sets2x2 = ['CLL', 'EG-1', 'EG-2'];
   const setsPyra = ['L4E'];
 
