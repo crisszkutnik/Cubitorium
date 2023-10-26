31dc5a2011f87338f46090f88a34d9a49d2238eb
guidoenr <guidoenr4@gmail.com>
2023-10-26 17:08:27 -0300

    added bottom bar + notfound fix + improvements + bujias

---
+++ client/src/components/BottomBar.tsx
@@ -0,0 +1,10 @@
+
+export function BottomBar() {
+  return (
+    <div className="fixed bottom-0 left-0 w-full h-5 bg-gray-900 text-gray-100 flex items-center justify-center text-xs">
+      @Cubitorium by Guido Dipietro -  Cristobal Szkutnik - Guido Enrique - Matias Davicino - Agustin Villareal
+    </div>
+  );
+};
+
+export default BottomBar;
--- client/src/pages/NotFound.tsx
+++ client/src/pages/NotFound.tsx
@@ -1,11 +1,11 @@
 export function NotFound() {
     return (
-      <div className="w-full h-screen flex flex-col items-center justify-center -mt-20">
-        <img src="/rubik-sad.png" />
+      <div className="relative h-screen mt-20 text-center">
+        <img src="/rubik-sad.png" className="mx-auto" />
         <h1 className="text-accent-dark font-semibold text-4xl mt-4">
-          404 - Not Found
+          4<img className="inline-block h-10 grayscale" src="/public/Logo.png" alt="Cubitorium" />4 - Not Found
         </h1>
-        <h3 className="max-w-xs text-center mt-4">
+        <h3 className="max-w-xs text-center mt-4 mx-auto">
             Just like finding the right moves to solve a Rubik's Cube, sometimes it takes a little twist and turn to discover the right path.
         </h3>
         <h2 className="text-green-600 font-bold">Keep Searching!</h2>
