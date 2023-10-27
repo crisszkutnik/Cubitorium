dd3166854d6a807e32b0569aedda0be65d579e75
guidoenr <guidoenr4@gmail.com>
2023-10-27 15:28:59 -0300

    lo saque a la remil mierda

--- client/src/components/BottomBar.tsx
+++
@@ -1,9 +0,0 @@
-export function BottomBar() {
-  return (
-    <div className="fixed bottom-0 left-0 w-full h-8 bg-gray-500 text-black-500 flex items-center justify-center text-ms">
-      ©2023 Cubitorium - all rights reserved
-    </div>
-  );
-};
-
-export default BottomBar;
