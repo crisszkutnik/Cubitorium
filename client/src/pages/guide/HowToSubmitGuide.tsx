488f56d104c254ede5362a26045c7f0f55c44446
guidoenr <guidoenr4@gmail.com>
2023-10-29 15:05:30 -0300

    path

--- client/src/pages/guide/HowToSubmitGuide.tsx
+++ client/src/pages/guide/HowToSubmitGuide.tsx
@@ -15,7 +15,7 @@ export function HowToSubmitGuide() {
                                 <p className="text-md text-gray-600 mb-4 mr-6 max-w-xs text-right">
                                 Select a case on the  <Link href="/algorithms" className="text-sky-600 font-bold">Algorithms</Link> page to which you want to upload a new solution.
                                 </p>
-                                <img src="../../../public/step-01.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-2/5 w-2/4" />
+                                <img src="/public/step-01.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-2/5 w-2/4" />
                             </div>
                             <hr/>
                     </div>
@@ -26,7 +26,7 @@ export function HowToSubmitGuide() {
                     <div className="mt-10 mb-10">
                     <h2 className="text-2xl mb-3">Step 2: Add a solution for the selected case</h2>
                     <div className="mb-4 flex items-center justify-center">
-                    <img src="../../../public/step-02.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-3/5 w-4/4" />
+                    <img src="/public/step-02.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-3/5 w-4/4" />
                         <p className="text-md text-gray-600 mb-4 ml-6 max-w-xs text-left">
                             Once you select the case, click on the <b>+ Add </b>button in order to be redirected to the  <Link href="/algorithms/upload" className="text-sky-600 font-bold">Upload your Algorithm</Link> page .
                         </p>
@@ -51,7 +51,7 @@ export function HowToSubmitGuide() {
 
                             Remember to use the official <Link className="text-sky-600 font-bold" href="https://www.worldcubeassociation.org/regulations/#article-12-notation">WCA Notation.</Link>
                         </p>
-                        <img src="../../../public/step-03.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-80 w-500" />
+                        <img src="/public/step-03.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-80 w-500" />
                     </div>
                     <hr/>
                     </div>
@@ -62,7 +62,7 @@ export function HowToSubmitGuide() {
                     <h2 className="text-2xl mb-3 mr-20 text-right">Step 4: Rock the world</h2>
                     <div className="mb-4 flex items-center justify-center">
                         
-                        <img src="../../../public/step-04.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-80 w-500" />
+                        <img src="/public/step-04.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-80 w-500" />
                         <p className="text-md text-gray-600 mb-4 ml-6 max-w-xs text-left">
                         Start cubing!<br/>
                         You will be able to visualize the solution that you're typing in real time.
@@ -91,7 +91,7 @@ export function HowToSubmitGuide() {
                         
                         Admins set the limit, but don't worry, it's high enough to get you going.
                         </p>
-                        <img src="../../../public/step-05.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-80 w-500" />
+                        <img src="/public/step-05.jpeg" className="border-1 border-purple-800 mb-2 inline-block h-80 w-500" />
                     </div>
                     <hr/>
                     </div>
@@ -110,7 +110,7 @@ export function HowToSubmitGuide() {
                     
                     </p>
             
-                    <img src="../../../public/step-07.jpeg" className="border-1 border-purple-800 mb-2 inline-block max-w-6xl" />    
+                    <img src="/public/step-07.jpeg" className="border-1 border-purple-800 mb-2 inline-block max-w-6xl" />    
 
                     
                     <div className="-mt-20 items-center justify-center">
