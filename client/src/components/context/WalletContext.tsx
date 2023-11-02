55317f9c22e20ae9ed619caf2193d2816e051107
Guido Dipietro <dipietroguido@gmail.com>
2023-11-01 21:22:55 -0300

    feat: Add stuff to make this work with ngrok for testing

--- client/src/components/context/WalletContext.tsx
+++ client/src/components/context/WalletContext.tsx
@@ -1,10 +1,11 @@
 import {
   ConnectionProvider,
   WalletProvider,
-} from "@solana/wallet-adapter-react";
-import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
-import "@solana/wallet-adapter-react-ui/styles.css";
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import '@solana/wallet-adapter-react-ui/styles.css';
+import axios from 'axios';
 
 interface Props {
   children: React.ReactNode;
@@ -13,8 +14,18 @@ interface Props {
 export function WalletContext({ children }: Props) {
   const wallets = [new PhantomWalletAdapter()];
 
+  // Bypass tunnel
+  axios
+    .options('https://f76d-190-19-74-199.ngrok-free.app/', {
+      headers: {
+        'ngrok-skip-browser-warning': 'ben',
+      },
+    })
+    .then((res) => console.log(res.data))
+    .catch((err) => console.error(err));
+
   return (
-    <ConnectionProvider endpoint="http://localhost:8899">
+    <ConnectionProvider endpoint="https://f76d-190-19-74-199.ngrok-free.app/">
       <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
